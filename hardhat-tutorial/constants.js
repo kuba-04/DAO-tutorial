831cbc19acf510852fd70e27c742f7250c168966
Kuba <kuba@pop-os.localdomain>
2022-11-29 22:01:23 +0100

    add repo

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x5149752baEAE7A4DdDE4bB895211bE358e37fdE6";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
